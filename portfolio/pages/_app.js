efed851665cdadaa21bb4c40758759c2b023635a
Bharti Patel <patelshree40@gmail.com>
2024-01-09 19:36:07 +0530

    home and nav created

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css';
+import '../styles/global.css';
+import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+import { useRouter } from 'next/router';
+import {AnimationPresence, motion} from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimationPresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition/>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimationPresence>
+    </Layout>
+  );
 }
 
 export default MyApp;
